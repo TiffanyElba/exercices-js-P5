4485754aa46485ab86e0cc131ea2b72e709696df
Tiffany <tiffanyth.1812@gmail.com>
2024-10-07 13:08:23 +0200

    exercice 9 ok

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,18 @@
 // votre code ici
+function checkAge(age) {
+    if (age < 18) {
+        console.log('Vous êtes mineur.')
+    }
+
+    if (age >= 18 && age <= 65) {
+        console.log('Vous êtes majeur.')
+    }
+
+    if (age > 65) {
+        console.log('Vous êtes senior.')
+    }
+}
 
 export default checkAge;
+
+
